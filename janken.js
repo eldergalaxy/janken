a8808047ab929b5ce83bbc8d7cfa84be35a5d534
eldergalaxy <173822183+eldergalaxy@users.noreply.github.com>
2025-07-12 08:36:04 -0500

    Feat: adds playGame function adds Score Keeping

--- janken.js
+++ janken.js
@@ -1,15 +1,9 @@
 
-
-
 let humanScore = 0;
 let computerScore = 0;
 
-
-
-
 getComputerChoice = function(max){
     computerChoice =  Math.floor(Math.random() * max + 1);
-     parseInt(computerChoice);
     if (computerChoice === 1) {return "rock"};
     if (computerChoice === 2) {return "paper"};
     if (computerChoice === 3) {return "scissors"};
@@ -27,26 +21,35 @@ getHumanChoice = function(){
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'scissors'){
         console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
+        humanScore ++;
     }else if (humanChoice === 'paper' && computerChoice === 'rock'){
         console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
+        humanScore ++;
     }else if (humanChoice === 'scissors' && computerChoice === 'paper'){
         console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
-    
+        humanScore ++;
+        
     }else if (computerChoice === 'rock' && humanChoice === 'scissors'){
         console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
+        computerScore ++;
     }else if (computerChoice === 'paper' && humanChoice === 'rock'){
         console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
+        computerScore ++;
     }else if (computerChoice === 'scissors' && humanChoice === 'paper'){
         console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
-
+        computerScore ++;
+        
     }else{
         console.log('Both played ' + humanChoice)};
-}
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(3);
-
-playRound(humanSelection, computerSelection);
-// console.log('human choice ' + humanSelection);
-// console.log('computer choice ' + computerSelection);
+    }
+        
+    function playGame(){
+        for (let i = 0; i < 5; i++) {
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice(3);        
+            playRound(humanSelection, computerSelection);
+        }
+    }
+    
+playGame();
+console.log("computerScore = " + computerScore, "humanScore = " + humanScore);
