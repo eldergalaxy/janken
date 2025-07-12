a740fb5aa7719b9af83cc79935304f1bd70b1d61
eldergalaxy <173822183+eldergalaxy@users.noreply.github.com>
2025-07-11 20:55:54 -0500

    wip: redesign to assingment specs

--- janken.js
+++ janken.js
@@ -1,85 +1,52 @@
 
 
-function selectMove() {
-    const num = Math.floor(Math.random() *3) + 1;
-    if (num === 1) {
-        return {name: 'rock', moveVal: 1 };
-    } else if ( num === 2) {
-        return {name: 'paper', moveVal: 2};
-    } else {
-        return {name: 'scissors', moveVal: 3};
-    }
-    
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+getComputerChoice = function(max){
+    computerChoice =  Math.floor(Math.random() * max + 1);
+     parseInt(computerChoice);
+    if (computerChoice === 1) {return "rock"};
+    if (computerChoice === 2) {return "paper"};
+    if (computerChoice === 3) {return "scissors"};
+    return computerChoice;
 }
 
-// Create Players
-const player1 = {
-    victories: 0,
-    move: selectMove.name,
-    moveVal: selectMove.moveVal
-};
+getHumanChoice = function(){
+    choice = parseInt(prompt('1.(rock) 2.(paper) 3.(scissors)'));
+    if (choice === 1) {return 'rock'};
+    if (choice === 2) {return 'paper'};
+    if (choice === 3) {return 'scissors'};
+    return choice;
+}
 
-const player2 = {
-    victories: 0,
-    move: selectMove.name,
-    moveVal: selectMove.moveVal
-};
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
+    }else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
+    }else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        console.log('Human wins with ' + humanChoice, 'beating ' + computerChoice);
+    
+    }else if (computerChoice === 'rock' && humanChoice === 'scissors'){
+        console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
+    }else if (computerChoice === 'paper' && humanChoice === 'rock'){
+        console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
+    }else if (computerChoice === 'scissors' && humanChoice === 'paper'){
+        console.log('computer wins with ' + computerChoice, 'beating ' + humanChoice);
 
-
-
-
-let determineWinner = function() {
-    let winner = 'tie';
-    while (winner === 'tie'){
-        const move1 = selectMove();
-        const move2 = selectMove();
-        player1.move = move1.name;
-        player2.move = move2.name;
-        
-        if (player1.move === "rock" && player2.move === "scissors"){
-            winner = "player1";
-        } else if (player1.move === "paper" && player2.move === "rock"){
-            winner = "player1";
-        } else if (player1.move === "scissors" && player2.move === "paper"){
-            winner = "player1";
-        }else if (player1.move === player2.move){
-            winner = "tie";
-        }else{
-            winner = "player2";
-        }
-    }
-    return winner;
+    }else{
+        console.log('Both played ' + humanChoice)};
 }
 
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(3);
 
-
-for(i = 0; i<5; i++) {
-    const move1 = selectMove();
-    const move2 = selectMove();
-    player1.move = move1.name;
-    player2.move = move2.name;
-    
-    
-    console.log('Player1 chose: ' + player1.move)
-    console.log('Player2 chose: ' + player2.move)
-    console.log('Winner is: ' + determineWinner())
-    //upDateVictoryCounter()
-    // console.log('overall winner is1: ' + player1.victories)
-    // console.log('overall winner is2: ' + player2.victories)
-};
-
-
-
-
-
-
-
-
-
-// let whoWon = determineWinner();
-
-// console.log(whoWon);
-// let upDateVictoryCounter = function(whoWon) {
-//     console.log('testing');
-// }
+playRound(humanSelection, computerSelection);
+// console.log('human choice ' + humanSelection);
+// console.log('computer choice ' + computerSelection);
